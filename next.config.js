7cad9a20b96b539dd02aba8656d2dd89ed7f23ee
Benrobo <alumonabenaiah71@gmail.com>
2022-04-28 14:16:19 -0800

    added major changes

---
+++ next.config.js
@@ -0,0 +1,8 @@
+
+
+module.exports = {
+    reactStrictMode: true,
+    env: {
+      NAME: process.env.NAME,
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import { Layout } from "../components";
 
 
-
 export default function HomePage(){
 
     return (
         <div>
             <Layout>
-                
+                <h1>{process.env.NAME}</h1>
             </Layout>
         </div>
     )
