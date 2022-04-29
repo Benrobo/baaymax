33a5bb6fa83b02b61e1532a4278b87df0361d493
Benrobo <alumonabenaiah71@gmail.com>
2022-04-29 13:29:10 -0800

    added major changes

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,50 +11,55 @@ export default function Header({ children }) {
             <Container>
                 {children}
 
-                <div className={styles.left}>
-                    <div className={styles.top}>
-                        <span className={styles.badge}>
-                            Software Engineer
-                        </span>
-                        <br />
-                        <h1 className={styles.heading}>
-                            Making The Impossible Possible Using 1's and 0's.
-                        </h1>
-                        <br />
-                        <span className={styles.info}>
-                            Problem solving is what makes me unique.
-                        </span>
-                    </div>
-                    <div className={styles.bottom}>
-                        <div className={styles.bx}>
-                            <h1 className={styles.h1}>
-                                12
-                            </h1>
-                            <span>
-                                Years of Experience
+                <div className={styles.main}>
+
+                    <div className={styles.left}>
+                        <div className={styles.top}>
+                            <span className={styles.badge}>
+                                Software Engineer
                             </span>
-                        </div>
-                        <div className={styles.bx}>
-                            <h1 className={styles.h1}>
-                                120
+                            <br />
+                            <br />
+                            <h1 className={styles.heading}>
+                                Making The Impossible Possible. Using 1's and 0's.
                             </h1>
-                            <span>
-                                Project Completed
+                            <br />
+                            <span className={styles.info}>
+                                Problem solving is what makes me unique.
                             </span>
+                            <br />
                         </div>
-                    </div>
-                </div>
-                <div className={styles.right}>
-                    <div className={styles.mainArea}>
-                        <img src={avatar.src} className={styles.avatar} />
-                    </div>
-                    <div className={styles.circleA}>                
-                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" className={styles.langImgA} />
-                        <div className={styles.circleB}>
-                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" className={styles.langImgB} />
-                            <div className={styles.circleC}>
-                                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" className={styles.langImgC} />
+                        <div className={styles.bottom}>
+                            <div className={styles.bx}>
+                                <h1 className={styles.h1}>
+                                    12
+                                </h1>
+                                <span className={styles.span}>
+                                    Years of Experience
+                                </span>
                             </div>
+                            <div className={styles.bx}>
+                                <h1 className={styles.h1}>
+                                    120
+                                </h1>
+                                <span className={styles.span}>
+                                    Project Completed
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.mainArea}>
+                            <img src={avatar.src} className={styles.avatar} />
+                        </div>
+                        <div className={styles.circleA}>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" className={styles.langImgA} />
+                        </div>
+                        <div className={styles.circleB}>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" className={styles.langImgB} />
+                        </div>
+                        <div className={styles.circleC}>
+                            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" className={styles.langImgC} />
                         </div>
                     </div>
                 </div>
